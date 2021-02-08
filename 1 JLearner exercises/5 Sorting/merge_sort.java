ceb4ef789e8b97c7da9a5ab8704a05572bca7f07
JorritWillaert <randpyprojects@gmail.com>
2021-02-08 20:27:15 +0100

    Return a subarray of length b - a

--- 1 JLearner exercises/5 Sorting/merge_sort.java
+++ 1 JLearner exercises/5 Sorting/merge_sort.java
@@ -1,3 +1,11 @@
+int[] subarray(int[] array, int a, int b){
+	int[] subarray = new int[b - a];
+  	for (int i = 0; i < b - a; i++){
+    	subarray[i] = array[a + i];
+    }
+  return subarray;
+}
+
 int[] merge(int[] array1, int[] array2){
 	int[] merged_array = new int[array1.length + array2.length];
   	int index1 = 0;
@@ -5,8 +13,10 @@ int[] merge(int[] array1, int[] array2){
   	for (int i = 0; i < array1.length + array2.length; i++){
       if (index1 == array1.length){
       	fillInRest(merged_array, index1 + index2, index2, array2);
+        //JLearner does not include 'break', this would be faster to break now
       } else if (index2 == array2.length){
       	fillInRest(merged_array, index1 + index2, index1, array1);
+        //JLearner does not include 'break', this would be faster to break now
       } else {
             if (array1[index1] < array2[index2]){
                 merged_array[index1 + index2] = array1[index1];
