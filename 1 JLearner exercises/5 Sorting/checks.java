0055b63d03bc609246816afaa8eb49e40c00fde3
JorritWillaert <randpyprojects@gmail.com>
2021-02-08 14:07:20 +0100

    Check for permutations of arrays

--- 1 JLearner exercises/5 Sorting/checks.java
+++ 1 JLearner exercises/5 Sorting/checks.java
@@ -1,3 +1,16 @@
+boolean permutationOfOtherArray(int[] array1, int[] array2){
+	if (array1.length != array2.length){
+    	return false;
+    }
+  	for (int i = 0; i < array1.length; i++){
+      	int value = array1[i];
+    	if ((numberOfOccurences(array1, value) != numberOfOccurences(array2, value))){
+        	return false;
+        }
+    }
+  	return true;
+}
+
 int numberOfOccurences(int[] array, int value){
   	int count = 0;
 	for (int i = 0; i < array.length; i++){
