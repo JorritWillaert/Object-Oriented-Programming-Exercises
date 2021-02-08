4a8c3032c30de824f5cfdb1f0a7c2efa79e5c9b9
JorritWillaert <randpyprojects@gmail.com>
2021-02-08 19:37:42 +0100

    Implement insertion sort

--- 1 JLearner exercises/5 Sorting/insertion_sort.java
+++ 1 JLearner exercises/5 Sorting/insertion_sort.java
@@ -1,3 +1,10 @@
+void insertionSort(int[] array){
+	for (int n = 1; n < array.length; n++){
+      	int value = array[n];
+    	insert(array, n, value);
+    }
+}
+
 void insert(int[] array, int n, int value){
 	int i = 0;
   	while (array[i] < value){
