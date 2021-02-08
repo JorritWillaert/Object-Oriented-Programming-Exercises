618d42d253713ddc36c4afda0320adc23306c1cb
JorritWillaert <randpyprojects@gmail.com>
2021-02-08 13:55:02 +0100

    Check if array is already sorted

---
+++ 1 JLearner exercises/5 Sorting/checks.java
@@ -0,0 +1,11 @@
+boolean isSorted(int[] array){
+  	if (array.length == 1){
+    return true;
+    }
+	for (int i = 1; i < array.length; i++){
+    	if (array[i - 1] > array[i]){
+        	return false;
+        }
+    }
+  	return true;
+}
