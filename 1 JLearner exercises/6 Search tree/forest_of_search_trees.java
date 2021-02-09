540fdfe87d8de45ac3bf61e1f8e0e89d818e73bb
JorritWillaert <randpyprojects@gmail.com>
2021-02-09 16:58:26 +0100

    Return an array containing the sequence of children of a given node

--- 1 JLearner exercises/6 Search tree/forest_of_search_trees.java
+++ 1 JLearner exercises/6 Search tree/forest_of_search_trees.java
@@ -4,6 +4,30 @@ class TreeNode{
   	int value;
 }
 
+TreeNode[] sequenceOfChildren(TreeNode node){
+	TreeNode child = node.firstChild;
+  	if (child == null){
+    	return null;
+    }
+  	TreeNode[] old_sequence = new TreeNode[1];
+  	old_sequence[0] = child;
+  	int count = 1;
+  	while (child.nextSibling != null){
+    	TreeNode[] new_sequence = new TreeNode[old_sequence.length + 1];
+      	copy(new_sequence, old_sequence);
+      	new_sequence[count] = child.nextSibling;
+      	count++;
+      	child = child.nextSibling;
+      	old_sequence = new_sequence;
+    }
+  	return old_sequence;
+}
+
+void copy(TreeNode[] new_array, TreeNode[] old_array) {
+  for (int i = 0; i < old_array.length; i++)
+    new_array[i] = old_array[i];
+}
+
 int numberOfChilds(TreeNode node){
   	TreeNode child = node.firstChild;
   	if (child == null){
