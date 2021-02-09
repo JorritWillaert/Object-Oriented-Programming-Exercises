74b510874a86dc4c0fdc61912e85f2e1348769d8
JorritWillaert <randpyprojects@gmail.com>
2021-02-09 16:05:57 +0100

    Implement heapRemove by using sift-down

--- 1 JLearner exercises/5 Sorting/heap_sort.java
+++ 1 JLearner exercises/5 Sorting/heap_sort.java
@@ -1,9 +1,37 @@
-int[] heapAdd(int[] array, int n){
+int heapRemove(int[] array, int n){
+  	int root = array[0];
+  	array[0] = array[n - 1];
+  	int i = 0;
+  	int left_child = array[1];
+  	int right_child = array[2];
+	while (i <= (n - 1) / 2 && (array[i] < left_child || array[i] < right_child)){
+    	int parent = array[i];
+      	left_child = array[2 * i + 1];
+      	boolean right_OK = false;
+      	if (2 * i + 2 < n){
+      		right_OK = true;
+            right_child = array[2 * i + 2];
+        }
+      	if (right_OK == false || left_child > right_child){
+        	swap(array, i, 2 * i + 1);
+          	i = 2 * i + 1;
+        } else {
+        	swap(array, i, 2 * i + 2);
+          	i = 2 * i + 2;
+        }
+    }
+    return root;
+}
+
+void swap(int[] array, int parent, int child){
+	int child_value = array[child];
+    array[child] = array[parent];
+    array[parent] = child_value;
+}
+
+void heapAdd(int[] array, int n){
   	while (n != 0 && array[n] > array[(n - 1) / 2]){
-    	int child = array[n];
-      	array[n] = array[(n - 1) / 2];
-      	array[(n - 1) / 2] = child;
+    	swap(array, (n - 1) / 2, n);
       	n = (n - 1) / 2;
     }
-    return array;
 }
