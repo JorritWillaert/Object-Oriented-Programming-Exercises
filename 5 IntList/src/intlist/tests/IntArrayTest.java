b9f6a702b1f10409634109077a9293f039e524b3
JorritWillaert <randpyprojects@gmail.com>
2021-02-15 10:39:13 +0100

    Initial files for the implementation of intList (with arrays and linked lists)

---
+++ 5 IntList/src/intlist/tests/IntArrayTest.java
@@ -0,0 +1,14 @@
+package intlist.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class IntArrayTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ 5 IntList/src/intlist/tests/IntLinkedListTest.java
@@ -0,0 +1,14 @@
+package intlist.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class IntLinkedListTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
