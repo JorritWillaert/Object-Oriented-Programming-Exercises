9a8779baf5ae936d3f9f62cf54ca7baf2d3be408
JorritWillaert <randpyprojects@gmail.com>
2021-02-23 18:14:50 +0100

    Create the needed classes that all extends Intructions and make a simple testsuite

--- 9 InstructionProgram/src/instructionprogram/tests/InstructionProgramTest.java
+++ 9 InstructionProgram/src/instructionprogram/tests/InstructionProgramTest.java
@@ -4,11 +4,64 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class InstructionProgramTest {
+abstract class Instructions {}
 
+class LoadConstant extends Instructions {
+	public LoadConstant(int index, int constant) {
+		throw new AssertionError("Not yet implemented");
+	}
+}
+
+class Decrement extends Instructions {
+	public Decrement(int index) {
+		throw new AssertionError("Not yet implemented");
+	}
+}
+
+class Multiply extends Instructions {
+	public Multiply(int index1, int index2) {
+		throw new AssertionError("Not yet implemented");
+	}
+}
+
+class JumpIfZero extends Instructions {
+	public JumpIfZero(int index, int pcToGo) {
+		throw new AssertionError("Not yet implemented");
+	}
+}
+
+class Jump extends Instructions {
+	public Jump(int pcToGo) {
+		throw new AssertionError("Not yet implemented");
+	}
+}
+
+class Halt extends Instructions {
+	public Halt() {
+		throw new AssertionError("Not yet implemented");
+	}
+}
+
+class InstructionProgram {
+	private InstructionProgram(int[] registers){};
+	
+	public static void execute(int[] registers, Instructions[] instructions) {
+		throw new AssertionError("Not yet implemented");
+	}
+}
+
+class InstructionProgramTest {
+	
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		int[] registers = new int[32];
+		registers[0] = 4; //Base
+		registers[1] = 5; //Exponent
+		
+		Instructions[] instructions = {new LoadConstant(2, 1), new JumpIfZero(1, 5), new Multiply(2, 0), new Decrement(1), new Jump(1), new Halt()};
+		
+		InstructionProgram.execute(registers, instructions); //Calculate 4 to the power of 5
+		assertEquals(1024, registers[2]);
 	}
 
 }
