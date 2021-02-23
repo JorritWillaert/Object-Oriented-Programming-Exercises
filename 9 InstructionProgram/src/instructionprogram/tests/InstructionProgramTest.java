a6609d9243b8f363a9f4503e8cfb82e1d10890ff
JorritWillaert <randpyprojects@gmail.com>
2021-02-23 17:32:30 +0100

    Initial files for project 9 InstructionProgram

---
+++ 9 InstructionProgram/src/instructionprogram/tests/InstructionProgramTest.java
@@ -0,0 +1,14 @@
+package instructionprogram.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class InstructionProgramTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
