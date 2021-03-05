193b3cc404a6adc846db31859c670ec845f43a4b
JorritWillaert <randpyprojects@gmail.com>
2021-03-05 18:15:52 +0100

    Add testcases for an overriding equals method

--- 10 Lists/src/lists/tests/ListsTest.java
+++ 10 Lists/src/lists/tests/ListsTest.java
@@ -9,10 +9,18 @@ class ListsTest {
 	@Test
 	void test() {
 		EmptyList myEmptyList = new EmptyList();
+		EmptyList mySecondEmptyList = new EmptyList();
+		
+		assertEquals("[]", "" + myEmptyList);
+		assertEquals(myEmptyList, mySecondEmptyList);
+		
 		int[] ints = {1, 2, 3};
 		NonEmptyList myNonEmptyList = new NonEmptyList(ints);
+		int[] otherIntsObject = {1, 2, 3};
+		NonEmptyList mySecondNonEmptyList = new NonEmptyList(otherIntsObject);
 		
 		assertEquals("[1,2,3]", ""+ myNonEmptyList);
+		assertEquals(myNonEmptyList, mySecondNonEmptyList);
 	}
 
 }
