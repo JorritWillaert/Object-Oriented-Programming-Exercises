b64fbf37c9200f97acfdb5cd81656aad10d899eb
JorritWillaert <randpyprojects@gmail.com>
2021-03-15 09:42:27 +0100

    Solution of Lists exercise

--- 10 Lists/src/lists/tests/Lists.java
+++ 10 Lists/src/lists/tests/Lists.java
@@ -1,15 +1,31 @@
 package lists.tests;
 
 abstract class Lists {
-	public abstract String toString();
+	public String toString() {
+		String result = "[";
+		boolean first = true;
+		Lists list = this;
+		while (list instanceof NonEmptyList) {
+			NonEmptyList nel = (NonEmptyList) list;
+			if (first) 
+				first = false;
+			else
+				result += ",";
+			result += nel.getHead();
+			list = nel.getTail();
+		}
+		result += "]";
+		return result;
+	}
+	
+	public abstract int getLength();
 }
 
 class EmptyList extends Lists {
 	public EmptyList() {}
 	
-	@Override
-	public String toString() {
-		return "[]";
+	public int getLength() {
+		return 0;
 	}
 	
 	@Override
@@ -22,16 +38,17 @@ class EmptyList extends Lists {
 
 class NonEmptyList extends Lists {
 	
-	private int head;
-	private int[] tail;
+	/**
+	 * @invar | tail != null
+	 */
+	private final int head;
+	private final Lists tail;
 	
-	public NonEmptyList(int[] values) {
-		if (values.length == 0)
+	public NonEmptyList(int head, Lists tail) {
+		if (tail == null)
 			throw new IllegalArgumentException("The given values-array may not be empty.");
-		head = values[0];
-		if (values.length >= 1)
-			tail = new int[values.length - 1];
-			System.arraycopy(values, 1, tail, 0, tail.length);
+		this.head = head;
+		this.tail = tail;
 	}
 	
 	/**
@@ -44,41 +61,20 @@ class NonEmptyList extends Lists {
 	/**
 	 * @basic
 	 */
-	public int[] getTail() {
-		return tail.clone();
+	public Lists getTail() {
+		return tail;
 	}
 	
-	/**
-	 * @post | result == getTail().length + 1
-	 */
 	public int getLength() {
-		return getTail().length + 1;
+		return 1 + tail.getLength();
 	}
-	
-	@Override
-	public String toString() {
-		String string = "[" + head; 
-		for (int i = 0; i < tail.length; i++) {
-			string = string + "," + tail[i];
-		}
-		string += "]";
-		return string;
-	}
-	
+
 	@Override
 	public boolean equals(Object other) {
 		if (!(other instanceof NonEmptyList))
 			return false;
 		NonEmptyList otherNonEmptyList = (NonEmptyList) other;
-		if (otherNonEmptyList.getLength() != getLength())
-			return false;
-		if (otherNonEmptyList.getHead() != getHead())
-			return false;
-		int[] otherTail = otherNonEmptyList.getTail();
-		for (int i = 0; i < otherNonEmptyList.getLength() - 1; i++)
-			if (otherTail[i] != tail[i])
-				return false;
-		return true;
+		return head == otherNonEmptyList.head && tail.equals(otherNonEmptyList.tail);
 	}
 }
 
--- 10 Lists/src/lists/tests/ListsTest.java
+++ 10 Lists/src/lists/tests/ListsTest.java
@@ -14,13 +14,17 @@ class ListsTest {
 		assertEquals("[]", "" + myEmptyList);
 		assertEquals(myEmptyList, mySecondEmptyList);
 		
-		int[] ints = {1, 2, 3};
-		NonEmptyList myNonEmptyList = new NonEmptyList(ints);
-		int[] otherIntsObject = {1, 2, 3};
-		NonEmptyList mySecondNonEmptyList = new NonEmptyList(otherIntsObject);
-		
-		assertEquals("[1,2,3]", ""+ myNonEmptyList);
-		assertEquals(myNonEmptyList, mySecondNonEmptyList);
+		EmptyList l1 = new EmptyList();
+		NonEmptyList l2 = new NonEmptyList(30, l1);
+		NonEmptyList l3 = new NonEmptyList(20, l2);
+		NonEmptyList l4 = new NonEmptyList(10, l3);
+		assertEquals(10, l4.getHead());
+		assertEquals(l3, l4.getTail());
+		assertEquals(20, l3.getHead());
+		assertEquals(l2, l3.getTail());
+		assertEquals(3, l4.getLength());
+		assertEquals("[10,20,30]", l4.toString());
+		assertEquals(new NonEmptyList(10, new NonEmptyList(20, new NonEmptyList(30, new EmptyList()))), l4);
 	}
 
 }
