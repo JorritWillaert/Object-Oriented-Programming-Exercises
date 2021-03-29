4b035c837b3080401cdc70ca4eb595caf6f85a75
JorritWillaert <randpyprojects@gmail.com>
2021-03-29 12:22:14 +0200

    Implement ArrayMap

---
+++ 17 Collections/src/collections/ArrayMap.java
@@ -0,0 +1,89 @@
+package collections;
+
+import java.util.Arrays;
+
+class SimpleEntry implements Map.Entry {
+	private final Object key;
+	private final Object value;
+	
+	public SimpleEntry(Object key, Object value) {
+		this.key = key;
+		this.value = value;
+	}
+	
+	public Object getKey() {
+		return key;
+	}
+	
+	public Object getValue() {
+		return value;
+	}
+	
+	@Override
+	public boolean equals(Object other) {
+		if(!(other instanceof Map.Entry))
+			return false;
+		Map.Entry otherEntry = (Map.Entry)other;
+		return key.equals(otherEntry.getKey()) && value.equals(otherEntry.getValue());
+	}
+}
+
+public class ArrayMap implements Map {
+
+	/**
+	 * @invar | entries != null
+	 * @invar | Arrays.stream(entries.toArray()).allMatch(e -> e instanceof Entry)
+	 * @invar | Arrays.stream(entries.toArray()).map(e -> ((Entry)e).getKey()).distinct().count() == entries.size()
+	 * 
+	 * @representationObject
+	 */
+	private ArrayList entries = new ArrayList();
+	
+	/**
+	 * @post | size() == 0
+	 */
+	public ArrayMap() {}
+	
+	public Set entrySet() {
+		ArraySet result = new ArraySet();
+		for (int i = 0; i < entries.size(); i++) {
+			result.add(entries.get(i));
+		}
+		return result;
+	}
+	
+	public int size() {
+		return entries.size();
+	}
+	
+	public boolean containsKey(Object key) {
+		for (int i = 0; i < entries.size(); i++) {
+			if (((Entry)entries.get(i)).getKey().equals(key))
+				return true;
+		}
+		return false;
+	}
+	
+	public Object get(Object key) {
+		for (int i = 0; i < entries.size(); i++) {
+			if (((Entry)entries.get(i)).getKey().equals(key))
+				return ((Entry)entries.get(i)).getValue();
+		}
+		return null;
+	}
+	
+	public void put(Object key, Object value) {
+		if (!containsKey(key))
+			entries.add(new SimpleEntry(key, value));
+	}
+	
+	public void remove(Object key) {
+		for (int i = 0; i < entries.size(); i++) {
+			if (((Entry)entries.get(i)).getKey().equals(key)) {
+				entries.remove(i);
+				return;
+			}
+		}
+	}
+	
+}
