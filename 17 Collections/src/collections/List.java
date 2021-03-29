6099e25a223e619a2745fb730c6820c2fdd358d6
JorritWillaert <randpyprojects@gmail.com>
2021-03-29 09:12:47 +0200

    Project 17: Collections - List interface

---
+++ 17 Collections/src/collections/List.java
@@ -0,0 +1,69 @@
+package collections;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+/**
+ * Each instance of this interface stores a sequence of objects.
+ * 
+ * @invar | Arrays.stream(toArray()).allMatch(e -> e != null)
+ */
+public interface List {
+	
+	/**
+	 * @inspects | this
+	 * @basic
+	 * 
+	 * @post | result != null
+	 * 
+	 */
+	public Object[] toArray();
+	
+	/**
+	 * @inspects | this
+	 * @post | result == toArray().length
+	 */
+	public int size();
+	
+	/**
+	 * @inspects | this
+	 * @pre | 0 <= index && index < size()
+	 * @post | result == toArray()[index]
+	 */
+	public Object get(int index);
+	
+	/**
+	 * @inspects | this
+	 * @pre | object != null
+	 * @post | result == Arrays.stream(toArray()).anyMatch(e -> e.equals(object))
+	 */
+	public boolean contains(Object object);
+	
+	/**
+	 * @mutates | this
+	 * @pre | element != null
+	 * @post | Arrays.equals(toArray(), 0, size() - 1, old(toArray()), 0, old(size()))
+	 * @post | toArray()[old(size())] == element
+	 */
+	public void add(Object element);
+	
+	/**
+	 * @mutates | this
+	 * @pre | 0 <= index
+	 * @pre | index < size()
+	 * @post | size() == old(size()) - 1
+	 * @post | Arrays.equals(toArray(), 0, index, old(toArray()), 0, index)
+	 * @post | Arrays.equals(toArray(), index, size(), old(toArray()), index + 1, old(size()))
+	 */
+	public void remove(int index);
+	
+	/**
+	 * @mutates | this
+	 * @pre | 0 <= index
+	 * @pre | index < size()
+	 * @post | size() == old(size())
+	 * @post | IntStream.range(0, size()).allMatch(i -> toArray()[i] ==  (i == index ? element : old(toArray())[i]))
+	 */
+	public void set(int index, Object element);
+	
+}
