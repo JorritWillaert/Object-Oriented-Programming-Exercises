909cf23b6276bc88ee34f259a120c7248d08cbb9
JorritWillaert <randpyprojects@gmail.com>
2021-04-20 13:15:04 +0200

    Add testsuite for project Exams and Rooms

--- 22 Exams and Rooms/src/examsandroomsclient/ExamsAndRoomsTest.java
+++ 22 Exams and Rooms/src/examsandroomsclient/ExamsAndRoomsTest.java
@@ -2,13 +2,41 @@ package examsandroomsclient;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Set;
+
 import org.junit.jupiter.api.Test;
+
+import examsandrooms.Exam;
+import examsandrooms.Room;
 
 class ExamsAndRoomsTest {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		Exam exam1 = new Exam();
+		Exam exam2 = new Exam();
+		Exam exam3 = new Exam();
+		Exam exam4 = new Exam();
+		
+		Room room1 = new Room();
+		Room room2 = new Room();
+		
+		exam1.addRoom(room1);
+		room2.addExam(exam2);
+		
+		assertEquals(Set.of(room1), exam1.getRooms());
+		assertEquals(Set.of(room2), exam2.getRooms());
+		assertEquals(Set.of(exam1), room1.getExams());
+		assertEquals(Set.of(exam2), room2.getExams());
+		assertEquals(Set.of(), exam3.getRooms());
+		
+		exam3.addRoom(room1);
+		exam4.addRoom(room1);
+		exam1.addRoom(room2);
+		
+		assertEquals(Set.of(room1, room2), exam1.getRooms());
+		assertEquals(Set.of(exam1, exam3, exam4), room1.getExams());
+		assertEquals(Set.of(exam1, exam2), room2.getExams());
 	}
 
 }
