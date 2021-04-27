cd9f819055022661ea86fec6443f8d2a7b05c262
JorritWillaert <randpyprojects@gmail.com>
2021-04-27 09:32:28 +0200

    Possibility to make IteratorImpl a local class

--- 23 Iterators/src/iterators/ArrayList.java
+++ 23 Iterators/src/iterators/ArrayList.java
@@ -6,29 +6,28 @@ public class ArrayList implements Iterable {
 
 	private Object[] elements; //Now: can be made private - nested class has access to elements
 	
-	//Nested class --> Now a non-static (= inner) class
-	private class IteratorImpl implements Iterator {
-		
-		private int index;
-		
-		//Non-static (= inner) class --> Constructor takes implicit an arrayList as argument + implicit field of an ArrayList
-		private IteratorImpl() {} //You can also remove this default constructor
-		
-		@Override
-		public boolean hasNext() {
-			//return index < ArrayList.this.elements.length; //ArrayList.this: Reference to implicit field of ArrayList
-			return index < elements.length; //'elements' of ArrayList.this is implicitly used
-		}
-		
-		@Override
-		public Object next() {
-			//return ArrayList.this.elements[index++];
-			return elements[index++]; //'elements' of ArrayList.this is implicitly used
-		}
-	}
-
-	
 	public Iterator iterator() {
+		//Local class
+		class IteratorImpl implements Iterator {
+			
+			private int index;
+			
+			//Non-static (= inner) class --> Constructor takes implicit an arrayList as argument + implicit field of an ArrayList
+			private IteratorImpl() {} //You can also remove this default constructor
+			
+			@Override
+			public boolean hasNext() {
+				//return index < ArrayList.this.elements.length; //ArrayList.this: Reference to implicit field of ArrayList
+				return index < elements.length; //'elements' of ArrayList.this is implicitly used
+			}
+			
+			@Override
+			public Object next() {
+				//return ArrayList.this.elements[index++];
+				return elements[index++]; //'elements' of ArrayList.this is implicitly used
+			}
+		}
+		
 		return new IteratorImpl(); //'this' of the outer class (= an instance of ArrayList) is given to the constructor
 	}
 	
