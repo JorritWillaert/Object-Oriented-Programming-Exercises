023896d3e5b3ab3d8b6338da39b39688bb3667dc
JorritWillaert <randpyprojects@gmail.com>
2021-04-27 10:39:11 +0200

    Exclude the printing of a specific object with a lambda expression

--- 24 Internal Iterators/src/internal_iterators/ClientProgram.java
+++ 24 Internal Iterators/src/internal_iterators/ClientProgram.java
@@ -3,12 +3,12 @@ package internal_iterators;
 public class ClientProgram {
 	
 	//Previous lecture: external iterators - Now: Internal iterators. Every data structure must get a forEach method which takes a consumer object as data object
-	public static void printBoth(Iterable collection1, Iterable collection2) {
-		printAll(collection1);
-		printAll(collection2);
+	public static void printBoth(Object exclude, Iterable collection1, Iterable collection2) {
+		printAll(exclude, collection1);
+		printAll(exclude, collection2);
 	}
 	
-	public static void printAll(Iterable iterable) {
+	public static void printAll(Object exclude, Iterable iterable) {
 		
 		//Use an anonymous class instantiation
 //		iterable.forEach(new Consumer() {
@@ -21,9 +21,15 @@ public class ClientProgram {
 		
 		//Equivalent code as above (lambda-expression)
 		//iterable.forEach((Object value) -> {System.out.println(value);}); //We do not mention the interface Consumer, but Java determines it by itself (= target typing)
-		iterable.forEach(value -> System.out.println(value));
+		//iterable.forEach(value -> System.out.println(value));
 		//Only possible since the interface Consumer has only one abstract method --> Usage of lambda expression is possible
 		//Iterator has two abstract methods (hasNext() and next()) --> Not possible to implement with a lambda expression
+		
+		//Now: exclude an object
+		iterable.forEach(value -> {
+			if (!value.equals(exclude))
+				System.out.println(value);
+		});
 	}
 	
 }
